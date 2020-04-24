561aac9c41b9e25ee47ace3675ee0ab53bb51e4c
Sergey1116 <45533866+Sergey1116@users.noreply.github.com>
2020-04-24 22:32:55 +0300

    change_style

--- hw-36/src/components/AboutAXIT/CastomSection.js
+++ hw-36/src/components/AboutAXIT/CastomSection.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function CastomSection() {
     return (
         <div className="castom-section">
-            <h2>WHY THIS IS AWESOME</h2>
+            <h2>why this is awesome</h2>
             <div className="divider"></div>
             <p>Lorem ipsum dolor sit amet,   consectetur   adipiscing elit.</p>
         </div>
